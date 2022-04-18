c541171460827104616c13b66608d3c17eb042b8
bernawan <86337886+bernawan@users.noreply.github.com>
2022-04-18 14:47:59 +0700

    Senin_18 April 2022 (2)

--- chartdht11_flask.py
+++ chartdht11_flask.py
@@ -1,31 +1,18 @@
 import json
-
 import random
-
 import time
-
 import requests
-
 from datetime import datetime
-
-
-
 from flask import Flask, Response, render_template, stream_with_context
-
 
 
 application = Flask(__name__)
 
-random.seed()  # Initialize the random number generator
-
-
-
-
+random.seed()  # Initialize the random number generato
 
 @application.route('/')
 
 def index():
-
     return render_template('index.html')
 
 
@@ -33,35 +20,22 @@ def index():
 
 
 @application.route('/chart-data')
-
 def chart_data():
-
     def ambil_data_suhu():
-
         while True:
-
             response = requests.get('http://192.168.48.90:5000/suhu')
-
             suhu = json.loads(response.content)
-
             response.close()
-
             json_data = json.dumps(
-
                 {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value_suhu': suhu['suhu'], 'value_kelembapan': suhu['kelembapan']})
-
             yield f"data:{json_data}\n\n"
-
             time.sleep(1)
 
 
 
     response = Response(stream_with_context(ambil_data_suhu()), mimetype="text/event-stream")
-
     response.headers["Cache-Control"] = "no-cache"
-
     response.headers["X-Accel-Buffering"] = "no"
-
     return response
 
 
@@ -69,6 +43,5 @@ def chart_data():
 
 
 if __name__ == '__main__':
-
     application.run(host='0.0.0.0', debug=True, threaded=True)
 
